dbd1b406247d1cb0707641f6eeed6d7ada39e3a5
Denise Costa <denisecosta.codes@gmail.com>
2023-10-24 00:24:14 -0300

    fix: Movido para a pasta de components correta

--- src/coponents/common/Input/input.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyleInput = styled.input`
-  width:${props => props.width};
-  border-radius: 6px;
-  border: 1px solid #5EB1FF;
-  padding: 10px 20px;
-
-`
