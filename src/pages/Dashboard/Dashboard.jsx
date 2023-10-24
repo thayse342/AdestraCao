fa35c19ecfb10160fb74cbd42640c39fa73c26ae
DeniCosta <denisecosta.codes@gmail.com>
2023-10-23 22:34:59 -0300

    fix: Correção da pasta components

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+
+const Dashboard = () => {
+
+  return (
+      <></>
+
+  )
+}
+
+export default Dashboard
