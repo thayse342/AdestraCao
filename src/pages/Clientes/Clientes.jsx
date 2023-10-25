c0c78ae29ddd494e0202306289694a8b54edc58a
IgorSPinto <igorpinto103@gmail.com>
2023-10-25 00:29:38 -0300

    feat: :sparkles: Criação da página de clientes
    Criado a página para visualizar os clientes adcionados

---
+++ src/pages/Clientes/Clientes.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+import Search from '../../components/views/dashboard/Search/Search'
+
+
+const Clientes = () => {
+
+
+return (
+    <div>
+         <Layout >
+            <Search />
+         </Layout>
+    </div>
+)
+
+}
+
+export default Clientes
