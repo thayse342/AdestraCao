0ade91d5adc57618096299ad327966b156430c7c
Thayse342 <thayse342@gmail.com>
2023-10-25 16:18:46 -0300

    feat: :zap: Criada a seção historia da ladingpage

--- src/pages/Landingpage/Landinpage.jsx
+++ src/pages/Landingpage/Landinpage.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
-import Header from '../../components/views/landingpage/Header/Header'
-import StyleLanding from './landingpage.styles.js'
-
+import React from "react";
+import Header from "../../components/views/landingpage/Header/Header";
+import StyleLanding from "./landingpage.styles.js";
 
 const Landinpage = () => {
   return (
     <>
-    <Header/>
-    <StyleLanding>
-        <img className="cachorropata" src="/cachorro-pata.svg" />
-        <div className="content">
-         <div className='logolading'>
-            <img src="/cao-logo(1).svg" alt="logo" />
-            <h2>ADESTRA  <br />CÃO  </h2>
-            
-        </div>
-        <p>Nossa abordagem se baseia no respeito, na paciência e na comunicação positiva. Acreditamos que cada animal é único, e nosso objetivo é ajudar a construir um vínculo forte entre você e seu companheiro de quatro patas.</p>
-    
-       <p>Oferecemos treinamento personalizado para atender às necessidades individuais do seu pet, seja para obediência básica, solução de problemas comportamentais ou até mesmo truques divertidos.</p>
-        </div>
-        </StyleLanding>
+      <Header />
 
+      <StyleLanding>
+        <section className="home">
+          <img className="cachorropata" src="/cachorro-pata.svg" />
+          <div className="content">
+            <div className="logolading">
+              <img src="/cao-logo(1).svg" alt="logo" />
+              <h2>
+                ADESTRA <br />
+                CÃO{" "}
+              </h2>
+            </div>
+            <p>
+              Nossa abordagem se baseia no respeito, na paciência e na
+              comunicação positiva. Acreditamos que cada animal é único, e nosso
+              objetivo é ajudar a construir um vínculo forte entre você e seu
+              companheiro de quatro patas.
+            </p>
 
-    <section>
-        
-    </section>
-    
+            <p>
+              Oferecemos treinamento personalizado para atender às necessidades
+              individuais do seu pet, seja para obediência básica, solução de
+              problemas comportamentais ou até mesmo truques divertidos.
+            </p>
+          </div>
+        </section>
+      
+
+      <section className="historia">
+        <div className="containerHistoria">
+          <h2>História AdestraCão</h2>
+          <span>(Leonardo fundador)</span>
+          <p>
+            Sou um apaixonado adestrador de animais com anos de experiência no
+            treinamento e cuidado de diversas espécies de cães.
+          </p>
+          <p>
+            Minha jornada como adestrador começou com um profundo amor pelos
+            animais e um desejo de garantir que eles tenham vidas felizes e
+            saudáveis.
+          </p>
+          <p>
+            Vamos trabalhar juntos para fortalecer o relacionamento com seu pet
+            e garantir que cada dia seja uma experiência maravilhosa para ambos.
+          </p>
+        </div>
+        <img src="/dog-petisco.svg" alt="" />
+      </section>
+      </StyleLanding>
     </>
-  )
-}
+  );
+};
 
-export default Landinpage
+export default Landinpage;
--- src/pages/Landingpage/landingpage.styles.js
+++ src/pages/Landingpage/landingpage.styles.js
@@ -1,44 +1,70 @@
 import styled from "styled-components";
 
- const StyleLanding = styled.section `
- display: flex;
- align-items: center;
- justify-content: space-around;
- padding: 20px 50px;
- gap: 20px;
-
- background-color:#36E2FF;
-
-.cachorropata{
-    border-radius: 8px;
-}
-
-    .content{
-    width: 30%;
-    display: flex;
-    flex-direction: column;
-    }
-
-    .logolading{
+const StyleLanding = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  .home {
     display: flex;
     align-items: center;
     justify-content: space-around;
-    
-    }
-    h2{
+    gap: 20px;
+    background-color: #36e2ff;
+  }
+  .cachorropata {
+    border-radius: 8px;
+    padding: 50px;
+    width: 500px;
+  }
+
+  .content {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .logolading {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .logolading h2 {
     font-family: Paytone One;
     font-size: 20px;
     font-weight: 400;
     letter-spacing: 9px;
     text-align: center;
-    }
+  }
 
-    p{
-    text-align:justify;
+  p {
+    text-align: justify;
     font-family: Inter;
     font-size: 20px;
     padding: 10px 0;
-    
-    }
-`
-export default StyleLanding 
+  }
+  section.historia {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #fef699;
+  }
+
+  .containerHistoria {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 50px;
+    background-color: #e7e3b8;
+    border-radius: 8px;
+    margin-right: 10px;
+  }
+
+  .containerHistoria h2 {
+    font-family: Inter;
+    font-size: 36px;
+    font-weight: 600;
+  }
+`;
+
+export default StyleLanding;
