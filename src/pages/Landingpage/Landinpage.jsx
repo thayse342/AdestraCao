f03d0edfcd2a8b8351a2f4ee91586e9c12257d88
Thayse342 <thayse342@gmail.com>
2023-10-25 01:05:48 -0300

    feat: :sparkles: Criado a primeira seção da lading page

---
+++ src/pages/Landingpage/Landinpage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../../components/views/landingpage/Header/Header'
+import StyleLanding from './landingpage.styles.js'
+
+
+const Landinpage = () => {
+  return (
+    <>
+    <Header/>
+    <StyleLanding>
+        <img className='' src="/cachorro-pata.svg" />
+        <div>
+        <img src="/cao-logo(1)" alt="logo" />
+        <p>Nossa abordagem se baseia no respeito, na paciência e na comunicação positiva. Acreditamos que cada animal é único, e nosso objetivo é ajudar a construir um vínculo forte entre você e seu companheiro de quatro patas.
+    
+    Oferecemos treinamento personalizado para atender às necessidades individuais do seu pet, seja para obediência básica, solução de problemas comportamentais ou até mesmo truques divertidos.</p>
+        </div>
+        </StyleLanding>
+
+
+    <section>
+        
+    </section>
+    
+    </>
+  )
+}
+
+export default Landinpage
---
+++ src/pages/Landingpage/landingpage.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+ const StyleLanding = styled.section `
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ padding: 20px;
+`
+export default StyleLanding 
