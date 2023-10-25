dc9dda29634789556bf1e7a32396082f190ca074
Thayse342 <thayse342@gmail.com>
2023-10-25 14:10:28 -0300

    feat: :sparkles: Finalização da primeira seção

--- src/pages/Landingpage/Landinpage.jsx
+++ src/pages/Landingpage/Landinpage.jsx
@@ -8,12 +8,16 @@ const Landinpage = () => {
     <>
     <Header/>
     <StyleLanding>
-        <img className='' src="/cachorro-pata.svg" />
-        <div>
-        <img src="/cao-logo(1)" alt="logo" />
-        <p>Nossa abordagem se baseia no respeito, na paciência e na comunicação positiva. Acreditamos que cada animal é único, e nosso objetivo é ajudar a construir um vínculo forte entre você e seu companheiro de quatro patas.
+        <img className="cachorropata" src="/cachorro-pata.svg" />
+        <div className="content">
+         <div className='logolading'>
+            <img src="/cao-logo(1).svg" alt="logo" />
+            <h2>ADESTRA  <br />CÃO  </h2>
+            
+        </div>
+        <p>Nossa abordagem se baseia no respeito, na paciência e na comunicação positiva. Acreditamos que cada animal é único, e nosso objetivo é ajudar a construir um vínculo forte entre você e seu companheiro de quatro patas.</p>
     
-    Oferecemos treinamento personalizado para atender às necessidades individuais do seu pet, seja para obediência básica, solução de problemas comportamentais ou até mesmo truques divertidos.</p>
+       <p>Oferecemos treinamento personalizado para atender às necessidades individuais do seu pet, seja para obediência básica, solução de problemas comportamentais ou até mesmo truques divertidos.</p>
         </div>
         </StyleLanding>
 
--- src/pages/Landingpage/landingpage.styles.js
+++ src/pages/Landingpage/landingpage.styles.js
@@ -3,7 +3,42 @@ import styled from "styled-components";
  const StyleLanding = styled.section `
  display: flex;
  align-items: center;
- justify-content: space-between;
- padding: 20px;
+ justify-content: space-around;
+ padding: 20px 50px;
+ gap: 20px;
+
+ background-color:#36E2FF;
+
+.cachorropata{
+    border-radius: 8px;
+}
+
+    .content{
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    }
+
+    .logolading{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    
+    }
+    h2{
+    font-family: Paytone One;
+    font-size: 20px;
+    font-weight: 400;
+    letter-spacing: 9px;
+    text-align: center;
+    }
+
+    p{
+    text-align:justify;
+    font-family: Inter;
+    font-size: 20px;
+    padding: 10px 0;
+    
+    }
 `
 export default StyleLanding 
