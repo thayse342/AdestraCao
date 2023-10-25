a4be1c7662b5659362043617fc900c955b8fda7b
Thayse342 <thayse342@gmail.com>
2023-10-25 01:06:55 -0300

    refactor: finalização do header

--- src/components/views/landingpage/Header/Header.jsx
+++ src/components/views/landingpage/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HeaderContainer } from "./header.styles.js";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
--- src/components/views/landingpage/Header/header.styles.js
+++ src/components/views/landingpage/Header/header.styles.js
@@ -2,21 +2,18 @@ import styled from "styled-components"
 
 export  const HeaderContainer = styled.header`
 background-color: #19DEFF;
-display:flex;
-justify-content: space-around;
-width: 100%;
+
 padding: 15px;
-align-items: center;
+
 
 .litalinks{
-    width:fit-content;
-    gap: 24px;
     align-items: center;
     display: flex;
+    justify-content: space-around;
 }
 
  .link{
-    color: #000;
+font-family: Poppins;
 font-size: 24px;
 font-style: normal;
 font-weight: 700;
