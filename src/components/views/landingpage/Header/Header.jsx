b86c1b7814afa4b25f3d73f0d5e410633920f649
Thayse342 <thayse342@gmail.com>
2023-10-24 23:08:04 -0300

    feat: :sparkles: Criação do componente header

---
+++ src/components/views/landingpage/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HeaderContainer } from "./header.styles.js";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <nav>
+        <ul className="litalinks">
+          <li>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/">
+              + Informações
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/">
+              Contato
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/login">
+              Àrea do Adestrador
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  );
+};
+export default Header;
