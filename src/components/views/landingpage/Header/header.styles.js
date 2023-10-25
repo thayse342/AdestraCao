57a1ef72863fbe1d5ba8d8d4dccda1280c42e4be
Thayse342 <thayse342@gmail.com>
2023-10-24 23:09:11 -0300

    feat: :sparkles: estilização Componente header

---
+++ src/components/views/landingpage/Header/header.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export  const HeaderContainer = styled.header`
+background-color: #19DEFF;
+display:flex;
+justify-content: space-around;
+width: 100%;
+padding: 15px;
+align-items: center;
+
+.litalinks{
+    width:fit-content;
+    gap: 24px;
+    align-items: center;
+    display: flex;
+}
+
+ .link{
+    color: #000;
+font-size: 24px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+
+
+}
+`
