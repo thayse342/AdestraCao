f7e9359e91d880aff61699417bf30c982c97568f
dayane99 <daystefane99@gmail.com>
2023-10-25 01:04:29 -0300

    feat: :sparkles: Criado componente BannerFooter
    Criação do componente Banner Footer, que fará parte da Landing Page do projeto.

---
+++ src/components/common/Bannerfooter/Bannerfooter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleBannerfooter } from './bannerfooter.style'
+import Input from '../Input/Input'
+import Button from '../Button/Button'
+import { WhatsappLogo } from '@phosphor-icons/react'
+import Logo from '../Logo/Logo'
+
+const Bannerfooter = ({ onClick }) => {
+  return (
+    <StyleBannerfooter>
+      <div className="tituloESub">
+        <h1>ENTRE EM CONTATO</h1>
+        <p className='subtitulo'>E agende uma aula experimental</p>
+      </div>
+
+      <form>
+        <Input width={"540px"} height={"60px"} placeholder={"email@email.com"} />
+        <Button texto={"ENVIAR"} variant={"primary"} width={"100px"} onClick={() => console.log("ok")} fontSize={"16px"} />
+        <button className='whatslogo' onClick={onClick}><WhatsappLogo size={50} /></button>
+      </form>
+      <Logo className='logo' />
+      <p className='direitosresevados'>Todos os direitos reservados©</p>
+    </StyleBannerfooter>
+  )
+}
+
+export default Bannerfooter
