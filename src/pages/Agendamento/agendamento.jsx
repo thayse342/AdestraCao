7edc517b70f00322574eef807ca21b767f766ed0
IgorSPinto <igorpinto103@gmail.com>
2023-10-26 00:19:08 -0300

    feat: :sparkles: Adicionado as informação da API
    Adição de elementos da API para manipulação de dados

--- src/pages/Agendamento/agendamento.jsx
+++ src/pages/Agendamento/agendamento.jsx
@@ -1,21 +1,40 @@
-import React, { useState } from 'react'
-import Agendamentoview from '../../components/views/dashboard/Agendamento/Agendamento'
-import Layout from '../../components/shared/Layout/Layout'
-import Search from '../../components/views/dashboard/Search/Search'
-
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Agendamentoview from '../../components/views/dashboard/Agendamento/Agendamento';
+import Layout from '../../components/shared/Layout/Layout';
+import Search from '../../components/views/dashboard/Search/Search';
+import { getAgendamento } from '../../service/api';
 
 const Agendamento = () => {
+    const params = useParams();
+    const [listaAgendamento, setListaAgendamento] = useState([]);
 
+    async function handleBuscarAgendamento() {
+        const resposta = await getAgendamento();
+        setListaAgendamento(resposta); // Define a lista de agendamentos com base na resposta da API
+    }
 
-return (
-    <div>
-         <Layout >
-            <Search />
-            <Agendamentoview/>
-         </Layout>
-    </div>
-)
+    useEffect(() => {
+        handleBuscarAgendamento(); // Chama a função de busca ao montar o componente
+    }, []); // O segundo argumento vazio faz com que a função seja chamada apenas uma vez no início
 
-}
+    return (
+        <div>
+            <Layout>
+                <Search />
+                {listaAgendamento.map((agendamento) => (
+                    <Agendamentoview
+                        key={agendamento._id} // Certifique-se de usar a propriedade correta para a chave
+                        id={agendamento._id}
+                        cliente={agendamento.cliente}
+                        pet={agendamento.pet}
+                        data={agendamento.data}
+                        duracao={agendamento.duracao}
+                    />
+                ))}
+            </Layout>
+        </div>
+    );
+};
 
-export default Agendamento
+export default Agendamento;
