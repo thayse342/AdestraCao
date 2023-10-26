7d9223237b409a549bb694a3ac7b6339fc154aec
IgorSPinto <igorpinto103@gmail.com>
2023-10-26 00:18:00 -0300

    feat: :sparkles: Adição de elementos da API
    adicionado elementos da api para renderização na página

--- src/components/views/dashboard/Agendamento/Agendamento.jsx
+++ src/components/views/dashboard/Agendamento/Agendamento.jsx
@@ -1,44 +1,44 @@
-import {Trash, Pencil} from "@phosphor-icons/react";
-import React from 'react'
-import styled, { useTheme } from 'styled-components'
+import { Trash, Pencil, Check } from "@phosphor-icons/react";
+import React from 'react';
+import styled from 'styled-components';
 
-const Agendamentoview = () => {
-
-
-
-
-
+const Agendamentoview = ({ id, cliente, pet, data, duracao }) => {
   return (
-    <StylesAgendamento>
-      <p>Igor</p>
-      <p>40028922</p>
-      <p>igor@email.com</p>
-      <button><Pencil size={25} color='black'/></button>
-      <button><Trash size={25} color='black'/></button>
-    </StylesAgendamento>
-  )
-}
 
-export default Agendamentoview
+
+    <StylesAgendamento>
+      
+      {console.log(`ID: ${id}, Cliente: ${cliente}, Pet: ${pet}, Data: ${data}, Duração: ${duracao}`)}
+
+      <button><Check size={22} color='black' /></button>
+      <p>Cliente: {cliente}</p>
+      <p>Dog: {pet}</p>
+      <p>Data: {data}</p>
+      <p>Duração: {duracao}</p>
+      <button><Pencil size={25} color='black' /></button>
+      <button><Trash size={25} color='black' /></button>
+    </StylesAgendamento>
+  );
+};
+
+export default Agendamentoview;
 
 const StylesAgendamento = styled.li`
-    width: 100%;
-    padding: 12px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 8px;
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
 
-    > button {
-    background-color: transparent; /* Fundo branco */
-    border: 2px solid black; /* Borda verde médio */
+  > button {
+    background-color: transparent;
+    border: 2px solid black;
   }
 
-    button:hover {
+  button:hover {
     opacity: 1;
-    transform: scale(1.05); /* Efeito de escala suave no hover */
-}
-
-
-`
+    transform: scale(1.05);
+  }
+`;
