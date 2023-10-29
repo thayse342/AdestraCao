aaa470c999cc71a3b767115f20473884b6b672ab
Denise Costa <denisecosta.codes@gmail.com>
2023-10-29 13:37:49 -0300

    feat: Adiciona a estilização das mensagens de alerta e da estilização do link para redefinição de senha

--- src/pages/Login/login.styles.js
+++ src/pages/Login/login.styles.js
@@ -5,21 +5,51 @@ const LoginStyled = styled.div`
   width: 100%;
   height: 100vh;
   
-  .voltar{
+  .voltar {
     display: flex;
     flex-direction: column;
     padding: 15px;
     cursor: pointer;
   }
 
-  main{
+  .error-message {
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fad2e1;
+  border: 1px solid #7c193d;
+  color: #7c193d;
+  padding: 10px;
+  border-radius: 5px;
+  z-index: 1;
+  opacity: 1; 
+}
+
+.success-message {
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #EFE;
+  border: 1px solid #658666;
+  color: #658666;
+  padding: 10px;
+  border-radius: 5px;
+  z-index: 1;
+  opacity: 1; 
+}
+
+
+
+  main {
     display: flex;
     align-items: center;
     justify-content: space-evenly;
     height: 80%;
   }
 
-  .stylesLeft{
+  .stylesLeft {
     width: 30%;
     height: 90%;
     display: flex;
@@ -27,24 +57,28 @@ const LoginStyled = styled.div`
     align-items: center;
     justify-content: space-between;
     padding: 15px;
-    text-align: justify ;
+    text-align: justify;
   }
 
-  .stylesRight{
+  .stylesRight {
     width: 30%;
     height: 80%;
     background-color: #f2f2f2;
     display: flex;
-    justify-content:center;
+    justify-content: center;
     border-radius: 15px;
   }
 
-  form{
+  form {
     height: 100%;
-    width:80%;
+    width: 80%;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
+  }
+
+  .password-reset-link{
+    text-decoration: underline;
   }
 `;
 
