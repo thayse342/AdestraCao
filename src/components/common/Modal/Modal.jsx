5eb9eeaa8b50007f1c5d15537eb8ae923f6005da
Denise Costa <denisecosta.codes@gmail.com>
2023-10-29 18:02:57 -0300

    fix: correção do modal

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -1,65 +1,48 @@
-import React from "react";
-import { StyleModal } from "./modal.style";
-import Button from "../Button/Button";
+import React, { Children } from 'react'
+import { StyleModal } from './modal.style'
+import Button from '../Button/Button'
 
-const Modal = ({
-  isOpen,
-  children,
-  setCloseModal,
-  botaosalvar,
-  titulo,
-  mensagemconfirmacao,
-  isModalOnLogin,
-}) => {
-  const closeModalAndExecuteAction = async () => {
-    if (isModalOnLogin) {
-      const success = await botaosalvar(); 
-      if (success) {
-        setCloseModal(); 
-      }
-    } else {
-      setCloseModal();
-    }
-  };
+const Modal = ({ isOpen, children, setCloseModal, botaosalvar, titulo, mensagemconfirmacao }) => {
 
   if (isOpen) {
-    return (
-      <StyleModal>
-        <div className="modal">
-          <div className="header">
-            <p>{titulo}</p>
+    return <StyleModal>
+      <div className='modal'>
+        <div className="header">
+          <p>{titulo}</p>
+        </div>
+        <div className="content">
+          {children}
+        </div>
+
+        <div className="footer">
+
+          <div className="esquerda">
+            <p>{mensagemconfirmacao}</p>
           </div>
-          <div className="content">{children}</div>
 
-          <div className="footer">
-            <div className="esquerda">
-              <p>{mensagemconfirmacao}</p>
-            </div>
+          <div className="direita">
+            <Button
+              texto={"salvar"}
+              variant={"primary"}
+              width={"65px"}
+              height={"35px"}
+              onClick={botaosalvar}
+              fontSize={"12px"}
+            />
+            <Button
+              texto={"Cancelar"}
+              variant={"secondary"}
 
-            <div className="direita">
-              <Button
-                texto={isModalOnLogin ? "Enviar" : "Salvar"}
-                variant={"primary"}
-                width={"65px"}
-                height={"35px"}
-                onClick={closeModalAndExecuteAction}
-                fontSize={"12px"}
-              />
-              <Button
-                texto={"Cancelar"}
-                variant={"secondary"}
-                width={"65px"}
-                height={"35px"}
-                onClick={setCloseModal}
-                fontSize={"12px"}
-              />
-            </div>
+              width={"65px"}
+              height={"35px"}
+              onClick={setCloseModal}
+              fontSize={"12px"} />
           </div>
         </div>
-      </StyleModal>
-    );
+      </div>
+    </StyleModal>
   }
-  return null;
-};
+  return null
+}
 
-export default Modal;
+export default Modal
