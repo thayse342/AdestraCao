24d27cc30042bd6607f410425039f238642b89fb
Denise Costa <denisecosta.codes@gmail.com>
2023-10-29 18:04:39 -0300

    feat: Estilização do componente Agendamento

---
+++ src/pages/Agendamento/agendamento.styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const AgendamentoStyled = styled.div`
+padding: 25px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+gap: 15px;
+
+ .inputContainer{
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: 25px;
+  
+ }
+
+ .inputItem{
+    width: 45%;
+ }
+`;
+
+export default AgendamentoStyled;
