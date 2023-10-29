d8e3c7ee210f4df16310fa9c5f4b9df285c15c92
Denise Costa <denisecosta.codes@gmail.com>
2023-10-29 16:11:52 -0300

    feat: Adiciona a propriedade placeholde ao componente input

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { StyleInput } from './input.style'
 
-const Input = ({ width, height, placeholder, type, value, onChange }) => {
+const Input = ({ width, height, placeholder, type, value, onChange, label }) => {
   return (
-    <StyleInput width={width} placeholder={placeholder} height={height} type={type} value={value} onChange={onChange} />
+    <StyleInput width={width} placeholder={placeholder} height={height} type={type} value={value} onChange={onChange} label={label} />
   )
 }
 
